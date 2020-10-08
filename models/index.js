b13ee80f3b3a8da51a0ac496b3754b60fd6ddcfc
Michael Law <michaellaw@gmx.com>
2020-10-07 23:37:50 -0400

    Wrote models for database except Item

--- models/index.js
+++ models/index.js
@@ -1,129 +1,228 @@
-const { Sequelize, DataTypes } = require('sequelize')
+const {
+  Sequelize,
+  DataTypes: {
+    STRING,
+    INTEGER,
+    DATE,
+    ENUM,
+    JSONB,
+    ARRAY,
+    BOOLEAN
+  }
+} = require('sequelize')
 const sequelize = new Sequelize('postgres://mlaw:cjiaang11@localhost/tacticsemblem')
 
 const User = sequelize.define('User', {
   username: {
-    type: DataTypes.STRING,
+    type: STRING,
     allowNull: false
   },
   email: {
-    type: DataTypes.STRING,
+    type: STRING,
     allowNull: false
   },
   password: {
-    type: DataTypes.STRING,
+    type: STRING,
     allowNull: false
   },
   birthday: {
-    type: DataTypes.DATE
+    type: DATE
   },
   bio: {
-    type: DataTypes.STRING
+    type: STRING
   },
   rank: {
-    type: DataTypes.ENUM,
+    type: ENUM,
     values: ['Beginniner', 'Novice Adventurer', 'Adventurer', 'Warrior']
   },
   zodiac: {
-    type: DataTypes.ENUM,
+    type: ENUM,
     values: ['Capricorn', 'Cancer', 'Libra', 'Saggitarius']
   },
   exp: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     defaultValue: 0
   },
   metadata: {
-    type: DataTypes.JSONB,
+    type: JSONB,
     defaultValue: {}
   }
 })
 
 const Unit = sequelize.define('Unit', {
   name: {
-    type: DataTypes.STRING,
+    type: STRING,
     allowNull: false
   },
   gender: {
-    type: DataTypes.ENUM,
+    type: ENUM,
     values: ['Male', 'Female', 'None']
   },
   zodiac: {
-    type: DataTypes.ENUM,
+    type: ENUM,
     values: ['Capricorn', 'Cancer', 'Libra', 'Saggitarius']
   },
   maxHp: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     defaultValue: 100
   },
   maxMp: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     defaultValue: 10
   },
   strength: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     defaultValue: 10
   },
   magicAtk: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     defaultValue: 8
   },
   equipment: {
-    type: DataTypes.JSONB,
+    type: JSONB,
     defaultValue: {
       right: null, left: null, helmet: null, body: null, shoes: null, accessory: null
     }
   },
   level: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     defaultValue: 1
   },
   moveRange: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     defaultValue: 3
   },
   bravery: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     default: 85
   },
   faith: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     default: 75
   },
   exp: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     default: 0
   },
   jp: {
-    type: DataTypes.JSONB,
+    type: JSONB,
     default: {Squire: 0}
   }
 })
 
 const Discipline = sequelize.define('Disciplines', {
   name: {
-    type: DataTypes.STRING,
+    type: STRING,
     allowNull: false
   },
   abilities: {
-    type: DataTypes.JSONB
+    type: JSONB
   }
 })
 
 const Ability = sequelize.define('Abilities', {
   name: {
-    type: DataTypes.STRING,
+    type: STRING,
     allowNull: false
   },
   damage: {
-    type: DataTypes.INTEGER,
+    type: INTEGER,
     defaultValue: 0
   },
+  range: {
+    type: INTEGER,
+    defaultValue: 1
+  },
+  jpCost: {
+    type: INTEGER,
+    defaultValue: 0
+  },
+  mpCost: {
+    type: INTEGER,
+    defaultValue: 0
+  },
+  ctCost: {
+    type: INTEGER,
+    defaultValue: 0
+  },
+  healing: {
+    type: BOOLEAN,
+    default: false
+  },
+  unitIds: {
+    type: ARRAY,
+    defaultValue: []
+  }
 })
 
-const Region = sequelize.define('Region', {
+const Job = sequelize.define('Jobs', {
   name: {
-    type: DataTypes.STRING,
+    type: STRING,
+    allowNull: false
+  },
+  discipline: {
+    type: STRING,
+    allowNull: false
+  },
+  baseStats: {
+    type: JSONB,
+    defaultValue: {
+      maxHp: 0,
+      atk: 0,
+      speed: 0,
+      moveRange: 0
+    }
+  }
+})
+
+const Equipment = sequelize.define('Equipment', {
+  name: {
+    type: STRING,
+    allowNull: false
+  },
+  metadata: {
+    type: JSONB,
+    defaultValue: {
+      type: 'Armor'
+    }
+  },
+  atk: {
+    type: INTEGER
+  },
+  hp: {
+    type: INTEGER
+  },
+  speed: {
+    type: INTEGER
+  },
+  moveRange: {
+    type: INTEGER
+  },
+  jobIds: { // Jobs that can equip this item
+    type: ARRAY,
+    defaultValue: []
+  },
+  status: { // determines things like auto-haste, auto-revive, auto-reflect, etc)
+    type: JSONB,
+    defaultValue: null
+  }
+})
+
+const Region = sequelize.define('Regions', {
+  name: {
+    type: STRING,
     allowNull: false
   }
 })
 
+module.exports = {
+  User: User,
+  Unit: Unit,
+  Discipline: Discipline,
+  Ability: Ability,
+  Job: Job,
+  Equipment: Equipment,
+  Region: Region
+}
+
+// TODO: Get Items from FFT Wiki
